13b1be775650a49f453749a320bb2288e7652320
covector <kinchancheuk@gmail.com>
2023-09-16 12:39:50 +0800

    fixed chrome update storage reset bug

--- wanikani-verb-reorder/background.js
+++ wanikani-verb-reorder/background.js
@@ -1,7 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({active: false});
-    chrome.action.setBadgeText({text: "OFF"});
-    chrome.action.setBadgeBackgroundColor({color: "#42495c"});
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === "install") {
+        chrome.storage.sync.set({active: false});
+        chrome.action.setBadgeText({text: "OFF"});
+        chrome.action.setBadgeBackgroundColor({color: "#42495c"});
+    } else {
+        chrome.storage.sync.get("active", ({ active }) => {
+            chrome.action.setBadgeText({text: active ? "ON" : "OFF"});
+            chrome.action.setBadgeBackgroundColor({color: "#42495c"});
+        });
+    }
 });
 
 chrome.action.onClicked.addListener(() => {
